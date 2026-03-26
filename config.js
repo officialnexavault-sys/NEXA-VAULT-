5858109c757420435045d067626eeb9d8f6b8356
officialnexavault-sys <official.nexavault@gmail.com>
2026-03-26 11:22:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,35 +1,30 @@
-// 🧠 NEXA VAULT - GLOBAL CONFIGURATION (V32.1)
-
 const CONFIG = {
     siteName: "NEXA VAULT",
     currency: "₹",
-    defaultGame: "ff", // सबसे पहले Free Fire खुलेगा
+    defaultGame: "ff",
     emptyStockMsg: "स्टॉक अभी खाली है, भाई 😏",
-    supportLink: "#", 
-    headerHeight: "65px"
+    supportLink: "#"
 };
 
-// 🎮 गेम की ID और उनके असली नाम (Professional Mapping)
 const GAME_NAMES = {
-    ff: "Free Fire", 
-    bgmi: "BGMI", 
-    cod: "COD Mobile", 
-    coc: "Clash Clans",
-    mc: "Minecraft", 
-    gta5: "GTA 5", 
-    asphalt: "Asphalt 9", 
-    roblox: "Roblox",
-    ludo: "Ludo King", 
-    pool: "8 Ball Pool", 
-    stumble: "Stumble Guys", 
-    val: "Valorant",
-    cs2: "CS2", 
-    mlbb: "Mobile Legends", 
-    genshin: "Genshin Impact", 
-    royale: "Clash Royale",
-    hok: "Honor of Kings", 
-    farlight: "Farlight 84", 
-    blood: "Blood Strike", 
-    mc_pc: "Minecraft PC"
+    ff: { name: "Free Fire", tag: "TRENDING" }, 
+    bgmi: { name: "BGMI", tag: "HOT" }, 
+    cod: { name: "COD Mobile", tag: "POPULAR" },
+    coc: { name: "Clash Clans", tag: "CLASSIC" },
+    mc: { name: "Minecraft", tag: "" },
+    gta5: { name: "GTA 5", tag: "" },
+    asphalt: { name: "Asphalt 9", tag: "" },
+    roblox: { name: "Roblox", tag: "" },
+    ludo: { name: "Ludo King", tag: "" },
+    pool: { name: "8 Ball Pool", tag: "" },
+    stumble: { name: "Stumble Guys", tag: "" },
+    val: { name: "Valorant", tag: "NEW" },
+    cs2: { name: "CS2", tag: "" },
+    mlbb: { name: "Mobile Legends", tag: "" },
+    genshin: { name: "Genshin Impact", tag: "" },
+    royale: { name: "Clash Royale", tag: "" },
+    hok: { name: "Honor of Kings", tag: "" },
+    farlight: { name: "Farlight 84", tag: "" },
+    blood: { name: "Blood Strike", tag: "" },
+    mc_pc: { name: "Minecraft PC", tag: "" }
 };
-
