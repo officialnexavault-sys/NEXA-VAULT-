3b1492f11e9b83d22a025528b7a64a7c10676c44
officialnexavault-sys <official.nexavault@gmail.com>
2026-03-26 10:58:01 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+const PRODUCTS = [
+    // ⬇️ ADMIN से कॉपी किया हुआ कोड यहाँ पेस्ट करें
+    { game: "ff", name: "Elite Pass ID - Lvl 72", price: "1,499" },
+    { game: "ff", name: "Old Sakura Bundle ID", price: "4,999" },
+    { game: "bgmi", name: "M416 Glacier + Mummy Set", price: "8,999" },
+    // ⬆️ पेस्ट खत्म
+];
+
