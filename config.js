45b3c52a6bd988a6f2088a790ef5f9620c16a0ee
officialnexavault-sys <official.nexavault@gmail.com>
2026-03-26 11:13:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,35 @@
+// 🧠 NEXA VAULT - GLOBAL CONFIGURATION (V32.1)
+
+const CONFIG = {
+    siteName: "NEXA VAULT",
+    currency: "₹",
+    defaultGame: "ff", // सबसे पहले Free Fire खुलेगा
+    emptyStockMsg: "स्टॉक अभी खाली है, भाई 😏",
+    supportLink: "#", 
+    headerHeight: "65px"
+};
+
+// 🎮 गेम की ID और उनके असली नाम (Professional Mapping)
+const GAME_NAMES = {
+    ff: "Free Fire", 
+    bgmi: "BGMI", 
+    cod: "COD Mobile", 
+    coc: "Clash Clans",
+    mc: "Minecraft", 
+    gta5: "GTA 5", 
+    asphalt: "Asphalt 9", 
+    roblox: "Roblox",
+    ludo: "Ludo King", 
+    pool: "8 Ball Pool", 
+    stumble: "Stumble Guys", 
+    val: "Valorant",
+    cs2: "CS2", 
+    mlbb: "Mobile Legends", 
+    genshin: "Genshin Impact", 
+    royale: "Clash Royale",
+    hok: "Honor of Kings", 
+    farlight: "Farlight 84", 
+    blood: "Blood Strike", 
+    mc_pc: "Minecraft PC"
+};
+
